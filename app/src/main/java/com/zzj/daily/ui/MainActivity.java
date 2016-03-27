0b543ac32b14ccbb21497816659e1ed214c9ba62
EoniJJ <397336190@qq.com>
2016-03-27 14:52:58 +0800

    update MainActivity

--- app/src/main/java/com/zzj/daily/ui/MainActivity.java
+++ app/src/main/java/com/zzj/daily/ui/MainActivity.java
@@ -121,19 +121,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         switch (id) {
             //点击 清除缓存
             case R.id.clear_cache:
@@ -163,7 +157,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *  实现按两下返回退出app
+     * 实现按两下返回退出app
+     *
      * @param keyCode
      * @param event
      * @return
